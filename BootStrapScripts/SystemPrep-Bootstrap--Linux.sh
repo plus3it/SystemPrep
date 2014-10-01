1ec1ec0e92a38b015b3ba18eee723a0a5bdcfd65
lorengordon <loren@fleet-it.com>
2014-10-01 15:07:30 -0400

    Updated bootstrap and master scripts
    Modified linux bootstrap and master scripts to use parameter splatting.
    Parameters are set in an array, then converted to a string to be passed
    to the master script, where they are re-interpreted as parameters.
    
    Changed some minor wording in a log message in the windows bootstrap
    scripts
    
    
    Former-commit-id: 3f22d6bbb76b64eb37d768360f29970b9630e522

--- BootStrapScripts/SystemPrep-Bootstrap--Linux.sh
+++ BootStrapScripts/SystemPrep-Bootstrap--Linux.sh
@@ -1,27 +1,31 @@
 #!/usr/bin/env bash
 set -ex
 
+#User variables
 SYSTEMPREPMASTERSCRIPTURL="https://systemprep.s3.amazonaws.com/MasterScripts/systemprep-linuxmaster.sh"
-declare -A SYSTEMPREPPARAMS=( ["SaltStates"]="Highstate" 
-                              ["NoReboot"]="False" )
-                              
-WORKINGDIR=/var/tmp/systemprep
-TIMESTAMP=$(date -u +"%Y%m%d_%H%M_%S")
-LOGFILE=${WORKINGDIR}/systemprep-log-${TIMESTAMP}.txt
+PARAMS=( "SaltStates=Highstate" 
+         "NoReboot=False" )
 
-mkdir ${WORKINGDIR}
+#System variables
+SCRIPTNAME=$0
+WORKINGDIR=/usr/tmp/systemprep
+TIMESTAMP=$(date -u +"%Y%m%d_%H%M_%S")
+LOGFILE=/var/log/systemprep-log-${TIMESTAMP}.txt
+
+if [[ ! -d "${WORKINGDIR}" ]] ; then mkdir ${WORKINGDIR} ; fi
 cd ${WORKINGDIR}
-echo "Entering SystemPrep Bootstrap script..." > ${LOGFILE}
-echo "Writing SystemPrep Parameters to log file..." >> ${LOGFILE}
-for key in "${!SYSTEMPREPPARAMS[@]}"; do echo "   ${key} = ${SYSTEMPREPPARAMS["$key"]}" >> ${LOGFILE} ; done
+echo "Entering SystemPrep script -- ${SCRIPTNAME}" &> ${LOGFILE}
+echo "Writing SystemPrep Parameters to log file..." &>> ${LOGFILE}
+for key in "${!SYSTEMPREPPARAMS[@]}"; do echo "   ${key} = ${SYSTEMPREPPARAMS["$key"]}" &>> ${LOGFILE} ; done
 
 SCRIPTFILENAME=$(echo ${SYSTEMPREPMASTERSCRIPTURL} | awk -F'/' '{ print ( $(NF) ) }')
 SCRIPTFULLPATH=${WORKINGDIR}/${SCRIPTFILENAME}
 
-echo "Downloading the SystemPrep script file -- ${SYSTEMPREPMASTERSCRIPTURL}" >> ${LOGFILE}
-curl -O -s ${SYSTEMPREPMASTERSCRIPTURL}
+echo "Downloading the SystemPrep script file -- ${SYSTEMPREPMASTERSCRIPTURL}" &>> ${LOGFILE}
+curl -O -s ${SYSTEMPREPMASTERSCRIPTURL} &>> ${LOGFILE}
 
 echo "Running the SystemPrep script -- ${SCRIPTFULLPATH}" >> ${LOGFILE}
-sh ${SCRIPTFULLPATH}
+PARAMSTRING=$( IFS=$' '; echo "${PARAMS[*]}" )
+sh ${SCRIPTFULLPATH} $PARAMSTRING &>> ${LOGFILE}
 
-echo "Exiting SystemPrep BootStrap script" >> ${LOGFILE}
+echo "Exiting SystemPrep script -- ${SCRIPTNAME}" &>> ${LOGFILE}
