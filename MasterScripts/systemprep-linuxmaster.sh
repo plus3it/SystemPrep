ad310521131edaf1cfce98f117f3d6260a4c234b
lorengordon <loren@fleet-it.com>
2014-10-01 19:33:21 -0400

    attempting hash tables in bash - not working

--- MasterScripts/systemprep-linuxmaster.sh
+++ MasterScripts/systemprep-linuxmaster.sh
@@ -12,16 +12,39 @@ READYFILE=/var/run/system-is-ready
 SCRIPTSTART="++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 SCRIPTEND="--------------------------------------------------------------------------------"
 
+#declare -A SCRIPTSTOEXECUTE
+SCRIPTSTOEXECUTE=( "https://systemprep.s3.amazonaws.com/SystemContent/Linux/Salt/SystemPrep-LinuxSaltInstall.sh" )
+SCRIPT0PARAMETERS=( "SALTWORKINGDIR=${WORKINGDIR}/systemcontent/linux/salt"
+                    "SALTCONTENTURL=https://systemprep.s3.amazonaws.com/SystemContent/Linux/Salt/salt-content.zip"
+                    "FORMULASTOINCLUDE=( 'https://salt-formulas.s3.amazonaws.com/ash-windows-formula-latest.zip' )" 
+                    "FORMULATERMINATIONSTRING=( '-latest' )" 
+                    "SALTSTATES=Highstate" )
+                                        
+                                        #Array of hashtables (key-value dictionaries). Each hashtable has two keys, ScriptUrl and Parameters. 
+
+
 if [[ ! -d "${WORKINGDIR}" ]] ; then mkdir ${WORKINGDIR} ; fi
-cd ${WORKINGDIR}
 
 echo "${SCRIPTSTART}"
 echo "Entering script -- ${SCRIPTNAME}"
-echo "Writing Parameters to log file..."
+echo "Printing parameters..."
 for PARAM in "${@}" ; do echo "   ${PARAM}" ; ${!PARAM} ; done
+echo "SaltStates = ${SaltStates}"
+echo "NoReboot = ${NoReboot}"
 
 if [[ -e ${READYFILE} ]] ; then rm -f ${READYFILE} ; fi
 
+cd ${WORKINGDIR}
+index=0
+for SCRIPT in ${SCRIPTSTOEXECUTE} ; do
+    curl -O -s ${SCRIPT}
+    SCRIPTFILENAME=$(echo ${SCRIPT} | awk -F'/' '{ print ( $(NF) ) }')
+    SCRIPTFULLPATH=${WORKINGDIR}/${SCRIPT}
+    PARAMSTRING=$( IFS=$' '; echo "${SCRIPT$(index)PARAMETERS[*]}" )
+    sh ./${SCRIPTFILENAME} $PARAMSTRING
+    index=(( $index + 1 ))
+done
+uyyhu 
 /bin/date > ${READYFILE}
 
 echo "Exiting SystemPrep script -- ${SCRIPTNAME}"
