3dc612fdce85512ccc64e59d242b82611a9341fe
MarionCobretti <the.marion.cobretti>
2014-10-02 14:11:10 -0400

    -Added cleanup function and call before end -Now if you raise an some kind of error call cleanup(workingdir) first.  Other items that need cleaned up can be added to that function as well.
    Former-commit-id: dca3108b59a282a8f26af44081eaac65bebb88f8

--- MasterScripts/systemprep-linuxmaster.py
+++ MasterScripts/systemprep-linuxmaster.py
@@ -1,31 +1,60 @@
-#!/usr/bin/env python 
-
+#!/usr/bin/env python
+import os
 import sys
+import platform
+import tempfile
 
-####################################################################################
-#Master Script that calls subscripts to be deployed to new Linux systems
-####################################################################################
+def cleanup(workingdir):
+    print('+-' * 40)
+    print('Cleanup Time...')
+    print('Removing temporary data...')
+    try:
+        os.removedirs(workingdir)
+    except:
+        raise SystemError('Cleanup Failed!')
 
-def main(*args,**kwargs) :
 
-    #System variables
+def main(**kwargs):
+    """
+Master Script that calls subscripts to be deployed to new Linux systems
+    """
+
     scriptname = sys.argv[0]
-    workingdir = '/usr/tmp/systemprep'
-    readyfile = '/var/run/system-is-ready'
-    scriptstart = '+' * 80
-    scriptend = '-' * 80
+    #Logic to define variables based on OS
+    if 'Windows' in platform.system():
+        #workingdir = 'C:\WINDOWS\TEMP\SYSTEMPREP'
+        workingdir = 'C:\WINDOWS\TEMP'
+        readyfile = 'C:\WINDOWS\TEMP\system-is-ready'
+    elif 'Linux' in platform.system():
+        #workingdir = '/usr/tmp/systemprep'
+        workingdir = '/usr/tmp/'
+        readyfile = '/var/run/system-is-ready'
+    else:
+        raise SystemError('platform.system() is not recognized?')
 
-    print scriptstart
-    print 'Entering script -- ', scriptname
-    print 'Printing parameters...'
+    print('+' * 80)
+    print('Entering script -- ' + scriptname)
+    if not os.path.exists(readyfile):
+        raise SystemError(readyfile + ' does not exist! System is not ready?')
 
-    print 'kwargs = ', kwargs
-    for key,value in kwargs.items() :
-        print ( str(key) + ' = ' + str(value) )
+    if os.path.exists(workingdir):
+        print('Creating working directory.')
+        workingdir = tempfile.mkdtemp(prefix='systemprep-', dir=workingdir)
+        print(workingdir)
+    else:
+        raise SystemError('Defined workingdir does not exist!')
 
+    print('Printing parameters...')
+    print('kwargs = ' + str(kwargs))
+
+    for key, value in kwargs.items():
+        print(str(key) + ' = ' + str(value))
+
+    cleanup(workingdir)
+    print('-' * 80)
 #    raw_input("\n\nPress the enter key to exit.")
 
-if __name__ == "__main__" :
-    kwargs = dict( x.split('=', 1) for x in sys.argv[1:] )
-    main(**kwargs)
 
+if __name__ == "__main__":
+    kwargs = dict(x.split('=', 1) for x in sys.argv[1:])
+    main(**kwargs)
