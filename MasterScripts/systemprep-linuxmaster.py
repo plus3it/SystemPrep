6d3d3ad545f57d650b96e95c342b9f1493059f9c
MarionCobretti <the.marion.cobretti>
2014-10-02 14:11:10 -0400

    -Removed stylistic commenting -Removed unused variables -Removed variables for formatting, this is an old approach and creates unnecessary variables which muddies the code.
    -Added pydoc comment
    -Added system check so script can be ported to any platform
    -Added tempdir creation, and cleanup so that files arent left behind
    -Added raise SystemErrors for unknown/undefined OS, the system not being ready, and no working directory.
    
    **Cleanup should be its own function so that if the script dies or exits due to error cleanup can be performed.  Thoughts?

--- MasterScripts/systemprep-linuxmaster.py
+++ MasterScripts/systemprep-linuxmaster.py
@@ -1,31 +1,57 @@
-#!/usr/bin/env python 
-
+#!/usr/bin/env python
+import os
 import sys
+import platform
+import tempfile
 
-####################################################################################
-#Master Script that calls subscripts to be deployed to new Linux systems
-####################################################################################
 
-def main(*args,**kwargs) :
+def main(**kwargs):
+    """
+Master Script that calls subscripts to be deployed to new Linux systems
+    """
 
-    #System variables
     scriptname = sys.argv[0]
-    workingdir = '/usr/tmp/systemprep'
-    readyfile = '/var/run/system-is-ready'
-    scriptstart = '+' * 80
-    scriptend = '-' * 80
+    #Logic to define variables based on OS
+    if 'Windows' in platform.system():
+        #workingdir = 'C:\WINDOWS\TEMP\SYSTEMPREP'
+        workingdir = 'C:\WINDOWS\TEMP'
+        readyfile = 'C:\WINDOWS\TEMP\system-is-ready'
+    elif 'Linux' in platform.system():
+        #workingdir = '/usr/tmp/systemprep'
+        workingdir = '/usr/tmp/'
+        readyfile = '/var/run/system-is-ready'
+    else:
+        raise SystemError('platform.system() is not recognized?')
 
-    print scriptstart
-    print 'Entering script -- ', scriptname
-    print 'Printing parameters...'
+    print('+' * 80)
+    print('Entering script -- ' + scriptname)
+    if not os.path.exists(readyfile):
+        raise SystemError(readyfile + ' does not exist! System is not ready?')
 
-    print 'kwargs = ', kwargs
-    for key,value in kwargs.items() :
-        print ( str(key) + ' = ' + str(value) )
+    if os.path.exists(workingdir):
+        print('Creating working directory.')
+        workingdir = tempfile.mkdtemp(prefix='systemprep-', dir=workingdir)
+        print(workingdir)
+    else:
+        raise SystemError('Defined workingdir does not exist!')
 
+    print('Printing parameters...')
+    print('kwargs = ' + str(kwargs))
+
+    for key, value in kwargs.items():
+        print(str(key) + ' = ' + str(value))
+
+    #TODO: Add Cleanup function and replace cleanup time. This does not cleanup if SystemError is raised after creation.
+    #todo Cleanup should be a function that is called during a SystemError so files are never left behind unless needed.
+
+    print('+-' * 40)
+    print('Cleanup Time...')
+    print('Removing temporary data...')
+    os.removedirs(workingdir)
+    print('-' * 80)
 #    raw_input("\n\nPress the enter key to exit.")
 
-if __name__ == "__main__" :
-    kwargs = dict( x.split('=', 1) for x in sys.argv[1:] )
-    main(**kwargs)
 
+if __name__ == "__main__":
+    kwargs = dict(x.split('=', 1) for x in sys.argv[1:])
+    main(**kwargs)
