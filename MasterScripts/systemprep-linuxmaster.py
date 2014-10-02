51864d97bc19ea46a4bc337c26f5794e31def2e9
lorengordon <loren@fleet-it.com>
2014-10-02 10:52:18 -0400

    read args into dict and print them
    Former-commit-id: d4961d65f82a99fddc989f3aead4b039dff0455b

---
+++ MasterScripts/systemprep-linuxmaster.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python 
+
+import sys
+
+####################################################################################
+#Master Script that calls subscripts to be deployed to new Linux systems
+####################################################################################
+
+def main(*args,**kwargs) :
+
+    #System variables
+    scriptname = sys.argv[0]
+    workingdir = '/usr/tmp/systemprep'
+    readyfile = '/var/run/system-is-ready'
+    scriptstart = '+' * 80
+    scriptend = '-' * 80
+
+    print scriptstart
+    print 'Entering script -- ', scriptname
+    print 'Printing parameters...'
+
+    print 'kwargs = ', kwargs
+    for key,value in kwargs.items() :
+        print ( str(key) + ' = ' + str(value) )
+
+#    raw_input("\n\nPress the enter key to exit.")
+
+if __name__ == "__main__" :
+    kwargs = dict( x.split('=', 1) for x in sys.argv[1:] )
+    main(**kwargs)
+
