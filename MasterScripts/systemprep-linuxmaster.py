8f617319e15c3e9342de236b218411de02f411c3
lorengordon <loren@fleet-it.com>
2014-10-08 11:25:35 -0400

    Updated docstrings, added noreboot default value
    noreboot defaults to false, meaning the system will reboot unless told
    not to.

--- MasterScripts/systemprep-linuxmaster.py
+++ MasterScripts/systemprep-linuxmaster.py
@@ -10,6 +10,9 @@ import shutil
 def mergeDicts(a, b):
     """
 Merge two dictionaries. If there is a key collision, `b` overrides `a`.
+    :param a: Dictionary of default settings
+    :param b: Dictionary of override settings
+    :rtype : dict
     """
     
     try:
@@ -23,16 +26,20 @@ Merge two dictionaries. If there is a key collision, `b` overrides `a`.
 def getScriptsToExecute(system, workingdir, **scriptparams):
     """
 Returns an array of hashtables. Each hashtable has two keys: 'ScriptUrl' and 'Parameters'.
-'ScriptUrl' is the url of the script to be downloaded and execute.
+'ScriptSource' is the path to the script to be executed. Only supports http/s sources currently.
 'Parameters' is a hashtable of parameters to pass to the script.
 Use `mergeDicts({yourdict}, scriptparams)` to merge command line parameters with a set of default parameters.
+    :param system: str, the system type as returned from `platform.system`
+    :param workingdir: str, the working directory where content should be saved
+    :param scriptparams: dict, parameters passed to the master script which should be relayed to the content scripts
+    :rtype : dict
     """
 
     if 'Linux' in system:
         scriptstoexecute = (
             { 
-                'ScriptUrl'  : "https://systemprep.s3.amazonaws.com/SystemContent/Linux/Salt/SystemPrep-LinuxSaltInstall.py",
-                'Parameters' : mergeDicts({ 
+                'ScriptSource'  : "https://systemprep.s3.amazonaws.com/SystemContent/Linux/Salt/SystemPrep-LinuxSaltInstall.py",
+                'Parameters'    : mergeDicts({
                                               'SaltWorkingPath' : workingdir + '/systemcontent/linux',
                                               'SaltContentUrl' : "https://systemprep.s3.amazonaws.com/SystemContent/Linux/Salt/salt-content.zip" ,
                                               'FormulasToInclude' : (
@@ -40,14 +47,14 @@ Use `mergeDicts({yourdict}, scriptparams)` to merge command line parameters with
                                                                     ),
                                               'FormulaTerminationStrings' : ( "-latest", ),
                                               'SaltStates' : 'Highstate',
-                                          }, scriptparams)
+                                             }, scriptparams)
             },
         )
     elif 'Windows' in system:
         scriptstoexecute = (
             {
-                'ScriptUrl'  : "https://systemprep.s3.amazonaws.com/SystemContent/Windows/Salt/SystemPrep-WindowsSaltInstall.ps1",
-                'Parameters' : mergeDicts({ 
+                'ScriptSource'  : "https://systemprep.s3.amazonaws.com/SystemContent/Windows/Salt/SystemPrep-WindowsSaltInstall.ps1",
+                'Parameters'    : mergeDicts({
                                               'SaltWorkingDir' : workingdir + '\\SystemContent\\Windows\\Salt',
                                               'SaltContentUrl' : "https://systemprep.s3.amazonaws.com/SystemContent/Windows/Salt/salt-content.zip",
                                               'FormulasToInclude' : (
@@ -57,7 +64,7 @@ Use `mergeDicts({yourdict}, scriptparams)` to merge command line parameters with
                                               'AshRole' : "MemberServer",
                                               'NetBannerString' : "Unclass",
                                               'SaltStates' : "Highstate",
-                                          }, scriptparams)
+                                             }, scriptparams)
             },
         )
     else:
@@ -71,6 +78,9 @@ def createWorkingDir(basedir, dirprefix):
 Creates a directory in `basedir` with a prefix of `dirprefix`.
 The directory will have a random 5 character string appended to `dirprefix`.
 Returns the path to the working directory.
+    :rtype : str
+    :param basedir: str, the directory in which to create the working directory
+    :param dirprefix: str, prefix to prepend to the working directory
     """
     workingdir = None
     try:
@@ -84,6 +94,8 @@ Returns the path to the working directory.
 def getSystemParams(system):
     """
 Returns a dictionary of OS platform-specific parameters.
+    :param system: str, the system type as returned by `platform.system`
+    :rtype : dict
     """
     
     a = {}
@@ -112,6 +124,9 @@ Returns a dictionary of OS platform-specific parameters.
 def downloadFile(url, filename):
     """
 Download the file from `url` and save it locally under `filename`:
+    :rtype : bool
+    :param url:
+    :param filename:
     """
     try:
         response = urllib2.urlopen(url)
@@ -123,23 +138,33 @@ Download the file from `url` and save it locally under `filename`:
             shutil.copyfileobj(response, outfile)
     except:
         raise SystemError('Unable to save file. \nfilename = \n    ' + filename)
-    
+
     print('Downloaded file -- \n    url      = ' + url + '\n    filename = ' + filename)
+    return True
 
 
 def cleanup(workingdir):
+    """
+    Removes temporary files loaded to the system.
+    :param workingdir: str, Path to the working directory
+    :return: bool
+    """
     print('+-' * 40)
     print('Cleanup Time...')
-    print('Removing temporary data...')
     try:
         shutil.rmtree(workingdir)
     except:
         raise SystemError('Cleanup Failed!')
 
+    print('Removed temporary data in working directory -- ' + workingdir)
+    print('Exiting cleanup routine...')
+    print('-+' * 40)
+    return True
 
-def main(**kwargs):
+
+def main( noreboot, **kwargs):
     """
-Master Script that calls subscripts to be deployed to new Linux systems
+    Master Script that calls content scripts to be deployed when provisioning systems
     """
 
     scriptname = __file__
@@ -156,26 +181,26 @@ Master Script that calls subscripts to be deployed to new Linux systems
     
     #Loop through each 'script' in scriptstoexecute
     for script in scriptstoexecute:
-        filename = script['ScriptUrl'].split('/')[-1]
+        filename = script['ScriptSource'].split('/')[-1]
         fullfilepath = systemparams['workingdir'] + systemparams['pathseparator'] + filename
-        #Download each script, script['ScriptUrl']
-        downloadFile(script['ScriptUrl'], fullfilepath)
+        #Download each script, script['ScriptSource']
+        downloadFile(script['ScriptSource'], fullfilepath)
         #Execute each script, passing it the parameters in script['Parameters']
         #TODO: figure out a better way to call and execute the script
-        print('Running script -- ' + script['ScriptUrl'])
+        print('Running script -- ' + script['ScriptSource'])
         print('Sending parameters --')
         for key, value in script['Parameters'].items():
             print('    ' + str(key) + ' = ' + str(value))
         paramstring = ' '.join("%s='%s'" % (key,val) for (key,val) in script['Parameters'].iteritems())
         fullcommand = 'python ' + fullfilepath + ' ' + paramstring
-        os.system(fullcommand) # likely a dirty hack, probably want to code
-                               # the python sub-script with an importable module instead
+        os.system(fullcommand) # likely a dirty hack, probably want to code the
+                               # python sub-script with an importable module instead
     
     cleanup(systemparams['workingdir'])
     
     #TODO: uncomment this when linux has a value for systemparams['restart']
-    if 'True' == kwargs['NoReboot']:
-        print('Detected NoReboot switch. System will not be rebooted.')
+    if 'True' == noreboot :
+        print('Detected noreboot switch. System will not be rebooted.')
     else:
         print('Reboot scheduled. System will reboot after the script exits.')
         os.system(systemparams['restart'])
@@ -184,6 +209,7 @@ Master Script that calls subscripts to be deployed to new Linux systems
     print('-' * 80)
 
 
-if __name__ == "__main__":
+if "__main__" == __name__ :
     kwargs = dict(x.split('=', 1) for x in sys.argv[1:])
-    main(**kwargs)
+    noreboot = kwargs.setdefault('NoReboot', 'False')
+    main(noreboot, **kwargs)
