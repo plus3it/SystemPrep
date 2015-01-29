896b768d8c8239230eeab3935dabf79c9afcb187
Loren Gordon <loren@fleet-it.com>
2015-01-29 18:06:00 -0500

    Support S3 bucket as a content source

--- TemplateScripts/SystemPrep-Bootstrap-Template-Windows.ps1
+++ TemplateScripts/SystemPrep-Bootstrap-Template-Windows.ps1
@@ -1,10 +1,12 @@
 #User variables
 $SystemPrepMasterScriptUrl = 'https://url/to/masterscript-windows.ps1'
+$SourceIsS3Bucket = $true
 $SystemPrepParams = @{
     Param1 = "Value1"
     Param2 = "Value2"
     Param3 = "Value3"
     Param4 = "Value4"
+    SourceIsS3Bucket = $SourceIsS3Bucket
 }
 
 #System variables
@@ -24,11 +26,34 @@ function log {
 	}
 }
 
+function Download-File {
+    [CmdLetBinding()]
+    Param(
+        [Parameter(Mandatory=$true,Position=0,ValueFromPipeLine=$true,ValueFromPipeLineByPropertyName=$true)] [string] $Url,
+        [Parameter(Mandatory=$true,Position=1,ValueFromPipeLine=$false,ValueFromPipeLineByPropertyName=$false)] [string] $SaveTo,
+        [Parameter(Mandatory=$false,Position=2,ValueFromPipeLine=$false,ValueFromPipeLineByPropertyName=$false)] [switch] $SourceIsS3Bucket
+    )
+    PROCESS {
+        if ($SourceIsS3Bucket) {
+            Write-Output "Downloading from S3 bucket and saving to: ${SaveTo}"
+            $SplitUrl = $Url.split('/') | where { $_ -notlike "" }
+            $BucketName = $SplitUrl[2]
+            $Key = $SplitUrl[3..($SplitUrl.count-1)] -join '/'
+            Read-S3Object -BucketName $BucketName -Key $Key -File $SaveTo 2>&1
+        }
+        else {
+            Write-Output "Downloading from HTTP host and saving to: ${SaveTo}"
+            New-Item "${SaveTo}" -ItemType "file" -Force > $null
+            (new-object net.webclient).DownloadFile("${Url}","${SaveTo}") 2>&1
+        }
+    }
+}
+
 if (-Not (Test-Path $SystemPrepDir)) { New-Item -Path $SystemPrepDir -ItemType "directory" -Force > $null; log "Created SystemPrep directory -- ${SystemPrepDir}" } else { log "SystemPrep directory already exists -- $SystemPrepDir" }
 $ScriptFileName = (${SystemPrepMasterScriptUrl}.split('/'))[-1]
 $ScriptFullPath = "${SystemPrepDir}\${ScriptFileName}"
 log "Downloading the SystemPrep master script -- ${SystemPrepMasterScriptUrl}"
-((new-object net.webclient).DownloadFile("${SystemPrepMasterScriptUrl}","${ScriptFullPath}") 2>&1) | log
+Download-File $SystemPrepMasterScriptUrl $ScriptFullPath -SourceIsS3Bucket:$SourceIsS3Bucket 2>&1 | log
 log "Running the SystemPrep master script -- ${ScriptFullPath}"
 (Invoke-Expression "& ${ScriptFullPath} @SystemPrepParams" 2>&1) | log
 log "Exiting SystemPrep BootStrap script"
