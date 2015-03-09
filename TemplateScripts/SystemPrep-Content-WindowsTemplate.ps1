4f376aa205458231e82adb669cbd707e4b6cf337
Loren Gordon <loren@fleet-it.com>
2015-03-09 08:57:57 -0400

    Update Windows Content script template
    - Add support to specify Aws region
    - Improve functions to better support the pipeline

--- TemplateScripts/SystemPrep-Content-WindowsTemplate.ps1
+++ TemplateScripts/SystemPrep-Content-WindowsTemplate.ps1
@@ -5,6 +5,9 @@ Param(
     ,
     [Parameter(Mandatory=$false,ValueFromPipeLine=$false,ValueFromPipeLineByPropertyName=$false)] 
     [switch] $SourceIsS3Bucket
+	,
+    [Parameter(Mandatory=$false,ValueFromPipeLine=$false,ValueFromPipeLineByPropertyName=$false)] 
+    [string] $AwsRegion
 
 #    ,
 #	[Parameter(Mandatory=$true,Position=0,ValueFromPipeLine=$false,ValueFromPipeLineByPropertyName=$false)] 
@@ -17,57 +20,69 @@ Param(
                       #This way, we don't need to know in advance all the parameter names for downstream scripts.
 
 #$SourceIsS3Bucket    #Set to $true if all content to be downloaded is hosted in an S3 bucket and should be retrieved using AWS tools.
+#$AwsRegion			  #Set to the region in which the S3 bucket is located.
 
 #$ParamName           #ParamDescription...
 
 #System variables
 $ScriptName = $MyInvocation.mycommand.name
-$SystemPrepDir = "${env:SystemDrive}\SystemPrep"
-$SystemPrepWorkingDir = $SystemPrepDir+"\WorkingFiles" #Location on the system to download the bucket contents.
 $SystemRoot = $env:SystemRoot
 $ScriptStart = "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 $ScriptEnd = "--------------------------------------------------------------------------------"
-$RemainingArgsHash = $RemainingArgs | ForEach-Object -Begin { $index = 0; $hash = @{} } -Process { if ($_ -match "^-.*:$") { $hash[($_.trim("-",":"))] = $RemainingArgs[$index+1] }; $index++ } -End { Write-Output $hash }
+#Convert RemainingArgs to a hashtable
+if ($PSVersionTable.PSVersion -eq "2.0") { #PowerShell 2.0 receives remainingargs in a different format than PowerShell 3.0
+	$RemainingArgsHash = $RemainingArgs | ForEach-Object -Begin { $index = 0; $hash = @{} } -Process { if ($index % 2 -eq 0) { $hash[$_] = $RemainingArgs[$index+1] }; $index++ } -End { Write-Output $hash }
+} else {
+	$RemainingArgsHash = $RemainingArgs | ForEach-Object -Begin { $index = 0; $hash = @{} } -Process { if ($_ -match "^-.*:$") { $hash[($_.trim("-",":"))] = $RemainingArgs[$index+1] }; $index++ } -End { Write-Output $hash }
+}###
 ###
 
 #User variables
 $PackageUrl = ""
-$TemplateWorkingDir = "${SystemPrepWorkingDir}\SystemContent\........."
-$PackageFile = (${PackageUrl}.split('/'))[-1]
+$TemplateWorkingDir = ""
 
 ###
 
 function log {
-	[CmdLetBinding()]
-	Param(
-		[Parameter(Mandatory=$true,Position=0,ValueFromPipeLine=$true,ValueFromPipeLineByPropertyName=$true)] [string[]] $LogMessage
-	)
-	PROCESS {
-		#Writes the input $LogMessage to the output for capture by the bootstrap script.
-		Write-Output "${Scriptname}: $LogMessage"
-	}
+    [CmdLetBinding()]
+    Param(
+        [Parameter(Mandatory=$true,Position=0,ValueFromPipeLine=$true,ValueFromPipeLineByPropertyName=$true)] [string[]] $LogMessage
+    )
+    PROCESS {
+		foreach ($message in $LogMessage) {
+			#Writes the input $LogMessage to the output for capture by the bootstrap script.
+			Write-Output "${Scriptname}: $message"
+		}
+    }
 }
 
 function Download-File {
     [CmdLetBinding()]
     Param(
-        [Parameter(Mandatory=$true,Position=0,ValueFromPipeLine=$true,ValueFromPipeLineByPropertyName=$true)] [string] $Url,
-        [Parameter(Mandatory=$true,Position=1,ValueFromPipeLine=$false,ValueFromPipeLineByPropertyName=$false)] [string] $SaveTo,
-        [Parameter(Mandatory=$false,Position=2,ValueFromPipeLine=$false,ValueFromPipeLineByPropertyName=$false)] [switch] $SourceIsS3Bucket
+        [Parameter(Mandatory=$true,Position=0,ValueFromPipeLine=$true,ValueFromPipeLineByPropertyName=$true)] [string[]] $Url,
+        [Parameter(Mandatory=$true,Position=1,ValueFromPipeLine=$false,ValueFromPipeLineByPropertyName=$false)] [string] $SavePath,
+        [Parameter(Mandatory=$false,Position=2,ValueFromPipeLine=$false,ValueFromPipeLineByPropertyName=$false)] [switch] $SourceIsS3Bucket,
+        [Parameter(Mandatory=$false,Position=3,ValueFromPipeLine=$false,ValueFromPipeLineByPropertyName=$false)] [string] $AwsRegion
     )
+	BEGIN {
+		New-Item -Path ${SavePath} -ItemType Directory -Force -WarningAction SilentlyContinue > $null
+	}
     PROCESS {
-        if ($SourceIsS3Bucket) {
-            Write-Output "Downloading from S3 bucket and saving to: ${SaveTo}"
-            $SplitUrl = $Url.split('/') | where { $_ -notlike "" }
-            $BucketName = $SplitUrl[2]
-            $Key = $SplitUrl[3..($SplitUrl.count-1)] -join '/'
-            Read-S3Object -BucketName $BucketName -Key $Key -File $SaveTo 2>&1
-        }
-        else {
-            Write-Output "Downloading from HTTP host and saving to: ${SaveTo}"
-            New-Item "${SaveTo}" -ItemType "file" -Force > $null
-            (new-object net.webclient).DownloadFile("${Url}","${SaveTo}") 2>&1
-        }
+		foreach ($url_item in $Url) {
+			$FileName = "${SavePath}\$((${url_item}.split('/'))[-1])"
+			if ($SourceIsS3Bucket) {
+				Write-Verbose "Downloading file from S3 bucket: ${url_item}"
+				$SplitUrl = $url_item.split('/') | where { $_ -notlike "" }
+				$BucketName = $SplitUrl[2]
+				$Key = $SplitUrl[3..($SplitUrl.count-1)] -join '/'
+				$ret = Invoke-Expression "Powershell Read-S3Object -BucketName $BucketName -Key $Key -File $FileName -Region $AwsRegion"
+			}
+			else {
+				Write-Verbose "Downloading file from HTTP host: ${url_item}"
+				(new-object net.webclient).DownloadFile("${url_item}","${FileName}")
+			}
+			Write-Output (Get-Item $FileName)
+		}
     }
 }
 
@@ -75,21 +90,23 @@ function Expand-ZipFile {
     [CmdLetBinding()]
     Param(
         [Parameter(Mandatory=$true,Position=0,ValueFromPipeLine=$true,ValueFromPipeLineByPropertyName=$true)] [string[]] $FileName,
-        [Parameter(Mandatory=$true,Position=1,ValueFromPipeLine=$false,ValueFromPipeLineByPropertyName=$false)] [string] $SourcePath,
-        [Parameter(Mandatory=$true,Position=2,ValueFromPipeLine=$false,ValueFromPipeLineByPropertyName=$false)] [string] $DestPath
+        [Parameter(Mandatory=$true,Position=1,ValueFromPipeLine=$false,ValueFromPipeLineByPropertyName=$false)] [string] $DestPath,
+        [Parameter(Mandatory=$false,Position=2,ValueFromPipeLine=$false,ValueFromPipeLineByPropertyName=$false)] [switch] $CreateDirFromFileName
     )
-    $Shell = new-object -com shell.application
-    if (!(Test-Path "$SourcePath\$FileName"))
-    {
-        throw "$SourcePath\$FileName does not exist" 
-    }
-    New-Item -ItemType Directory -Force -Path $DestPath -WarningAction SilentlyContinue 1> $null
-    $Shell.namespace($DestPath).copyhere($Shell.namespace("$SourcePath\$FileName").items(), 0x14) 
+    PROCESS {
+		foreach ($file in $FileName) {
+			$Shell = new-object -com shell.application
+			if (!(Test-Path "$file")) {
+				throw "$file does not exist" 
+			}
+			Write-Verbose "Unzipping file: ${file}"
+			if ($CreateDirFromFileName) { $DestPath = "${DestPath}\$((Get-Item $file).BaseName)" }
+			New-Item -Path $DestPath -ItemType Directory -Force -WarningAction SilentlyContinue > $null
+			$Shell.namespace($DestPath).copyhere($Shell.namespace("$file").items(), 0x14) 
+			Write-Output (Get-Item $DestPath)
+		}
+	}
 }
-
-#Make sure the system prep and working directories exist
-if (-Not (Test-Path $SystemPrepDir)) { New-Item -Path $SystemPrepDir -ItemType "directory" -Force 1>$null; log "Created SystemPrep directory -- ${SystemPrepDir}" } else { log "SystemPrep directory already exists -- $SystemPrepDir" }
-if (-Not (Test-Path $SystemPrepWorkingDir)) { New-Item -Path $SystemPrepWorkingDir -ItemType "directory" -Force 1>$null; log "Created working directory -- ${SystemPrepWorkingDir}" } else { log "Working directory already exists -- ${SystemPrepWorkingDir}" }
 
 #Create log entry to note the script that is executing
 log $ScriptStart
@@ -98,8 +115,8 @@ log "RemainingArgsHash = $(($RemainingArgsHash.GetEnumerator() | % { `"-{0}: {1}
 
 #Insert script commands
 ###
-Download-File -Url $PackageUrl -SaveTo "${TemplateWorkingDir}\${PackageFile}" | log
-Expand-ZipFile -FileName ${PackageFile} -SourcePath ${TemplateWorkingDir} -DestPath ${TemplateWorkingDir}
+$PackageFile = Download-File -Url $PackageUrl -SavePath "${TemplateWorkingDir}" -SourceIsS3Bucket:$SourceIsS3Bucket -AwsRegion $AwsRegion -Verbose
+$PackageDir = Expand-ZipFile -FileName ${PackageFile} -DestPath -DestPath ${TemplateWorkingDir} -Verbose
 
 ###
 
