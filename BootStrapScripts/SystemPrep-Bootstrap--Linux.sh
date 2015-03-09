a48dbac2a6b12b2f893c276ee50f144c79ceebfb
Loren Gordon <loren@fleet-it.com>
2015-03-09 19:41:44 -0400

    Rework linux bootstrap script
    - Add support for multiple downloaders: curl, wget, aws s3 tools
    - Add more error/prereq checks
    - Improve logging to write to a dedicated file and the system log

--- BootStrapScripts/SystemPrep-Bootstrap--Linux.sh
+++ BootStrapScripts/SystemPrep-Bootstrap--Linux.sh
@@ -2,37 +2,90 @@
 set -ex
 
 #User variables
-SYSTEMPREPMASTERSCRIPTSOURCE="https://systemprep.s3.amazonaws.com/MasterScripts/systemprep-linuxmaster.py"
+SOURCEISS3BUCKET="True"
+AWSREGION="us-east-1"
+SYSTEMPREPMASTERSCRIPTSOURCE="https://s3.amazonaws.com/systemprep/MasterScripts/systemprep-linuxmaster.py"
 SYSTEMPREPPARAMS=( "SaltStates=Highstate"
-                   "NoReboot=False" )
+                   "NoReboot=False"
+                   "SourceIsS3Bucket=${SOURCEISS3BUCKET}"
+                   "AwsRegion=${AWSREGION}" )
 
 #System variables
-SCRIPTNAME=$0
+SCRIPTNAME=${0}
+AWS=$(which aws)
+CURL=$(which curl)
+WGET=$(which wget)
+LOGGER=$(which logger)
+TIMESTAMP=$(date -u +"%Y%m%d_%H%M_%S")
 WORKINGDIR=/usr/tmp
 LOGDIR=/var/log
-TIMESTAMP=$(date -u +"%Y%m%d_%H%M_%S")
-LOGFILE=${LOGDIR}/systemprep-${TIMESTAMP}.log
+LOGTAG=systemprep
+LOGFILE="${LOGDIR}/${LOGTAG}-${TIMESTAMP}.log" # use double quotes to expand the variables immediately
+LOGCMD="${LOGGER} -t ${LOGTAG}" 
+TEELOG='tee -a ${LOGFILE} | ${LOGCMD}' # use single quotes to delay expansion until invocation
 
-if [[ ! -d "${LOGDIR}" ]] ; then mkdir ${LOGDIR} ; fi
-if [[ ! -d "${WORKINGDIR}" ]] ; then mkdir ${WORKINGDIR} ; fi
+# Validate logger exists
+if [[ (! -x ${LOGGER}) || (-z "${LOGGER}") ]]; then
+	echo "Can't find 'logger' in path. Quitting..."
+	exit
+fi
+
+# Validate log directory exists
+if [[ ! -d ${LOGDIR} ]]; then
+  echo "Creating ${LOGDIR} directory." 2>&1 | ${LOGCMD}
+  mkdir ${LOGDIR} 2>&1 | ${LOGCMD}
+fi
+
+# Validate working directory exists
+if [[ ! -d ${WORKINGDIR} ]]; then
+  echo "Creating ${WORKINGDIR} directory" 2>&1 | ${LOGCMD}
+  mkdir ${WORKINGDIR} 2>&1 | ${LOGCMD}
+fi
+
+# Change to the working directory
 cd ${WORKINGDIR}
-echo "Entering SystemPrep script -- ${SCRIPTNAME}" &> ${LOGFILE}
-echo "Writing SystemPrep Parameters to log file..." &>> ${LOGFILE}
-for param in "${SYSTEMPREPPARAMS[@]}"; do echo "   ${param}" &>> ${LOGFILE} ; done
 
+# Create the log file and write out the parameters
+echo "Entering SystemPrep script -- ${SCRIPTNAME}" 2>&1 | ${TEELOG}
+echo "Writing SystemPrep Parameters to log file..." 2>&1 | ${TEELOG}
+for param in "${SYSTEMPREPPARAMS[@]}"; do echo "   ${param}" 2>&1 | ${TEELOG} ; done
+
+# Download the master script
 SCRIPTFILENAME=$(echo ${SYSTEMPREPMASTERSCRIPTSOURCE} | awk -F'/' '{ print ( $(NF) ) }')
 SCRIPTFULLPATH=${WORKINGDIR}/${SCRIPTFILENAME}
+if [[ "true" = ${SOURCEISS3BUCKET,,} ]]; then
+    if [[ (-x ${AWS}) && (! -z ${AWS}) ]]; then
+        echo "Using AWS Tools to download from S3 Bucket -- ${SYSTEMPREPMASTERSCRIPTSOURCE}" 2>&1 | ${TEELOG}
+        KEY=$(echo ${SYSTEMPREPMASTERSCRIPTSOURCE} | awk -F'/' '{$1=$2=$3=""; print substr($0,4)}' OFS="/")
+        ${AWS} s3 cp s3://${KEY} ${SCRIPTFULLPATH} --source-region ${AWSREGION}
+    else
+        echo "Missing 'aws' in path. Could not download file. Quitting..." 2>&1 | ${TEELOG}
+        exit
+else
+    if [[ (-x ${CURL}) && (! -z ${CURL}) ]]; then
+        echo "Using 'curl' to download from web host -- ${SYSTEMPREPMASTERSCRIPTSOURCE}" 2>&1 | ${TEELOG}
+        ${CURL} -O -s -S ${SYSTEMPREPMASTERSCRIPTSOURCE} 2>&1 | ${TEELOG}
+    elif [[ (-x ${WGET}) && (! -z ${WGET}) ]]; then
+        echo "Using 'wget' to download from web host -- ${SYSTEMPREPMASTERSCRIPTSOURCE}" 2>&1 | ${TEELOG}
+        ${WGET} -O -s ${SYSTEMPREPMASTERSCRIPTSOURCE} 2>&1 | ${TEELOG}
+    else
+        echo "Missing 'curl' or 'wget' in path. Could not download file. Quitting..." 2>&1 | ${TEELOG}
+        exit
+fi
 
-echo "Downloading the SystemPrep master script -- ${SYSTEMPREPMASTERSCRIPTSOURCE}" &>> ${LOGFILE}
-curl -O -s ${SYSTEMPREPMASTERSCRIPTSOURCE} &>> ${LOGFILE}
-
-#Convert the parameter list to a string
-#The string will be converted to a python dictionary by the master script
+# Convert the parameter list to a string
+# The string will be converted to a python dictionary by the master script
 PARAMSTRING=$( IFS=$' '; echo "${SYSTEMPREPPARAMS[*]}" )
-echo "Running the SystemPrep master script -- ${SCRIPTFULLPATH}" >> ${LOGFILE}
+
+# Execute the master script
+SCRIPTFILENAME=$(echo ${SYSTEMPREPMASTERSCRIPTSOURCE} | awk -F'/' '{ print ( $(NF) ) }')
+SCRIPTFULLPATH=${WORKINGDIR}/${SCRIPTFILENAME}
+echo "Running the SystemPrep master script -- ${SCRIPTFULLPATH}" $>> ${LOGFILE}
 python ${SCRIPTFULLPATH} ${PARAMSTRING} &>> ${LOGFILE}
 
-echo "Deleting the SystemPrep master script -- ${SCRIPTFULLPATH}" >> ${LOGFILE}
+# Cleanup
+echo "Deleting the SystemPrep master script -- ${SCRIPTFULLPATH}" $>> ${LOGFILE}
 rm -f ${SCRIPTFULLPATH} &>> ${LOGFILE}
 
+# Exit
 echo "Exiting SystemPrep bootstrap script -- ${SCRIPTNAME}" &>> ${LOGFILE}
