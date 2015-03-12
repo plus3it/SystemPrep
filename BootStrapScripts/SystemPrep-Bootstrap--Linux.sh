b78db65951982e790de9b1731e07d17f3a8bf851
Loren Gordon <loren@fleet-it.com>
2015-03-12 09:22:08 -0400

    Streamline logging in Linux framework
    Also writes to the console, which is captured as the system log in EC2.
    This makes it easy to view the results from the AWS console or API.
    
    
    Former-commit-id: ce79e15d4b879139105d24a9e326834e4e3b347a
    Former-commit-id: edc4c740cbd3a869b16d5afdd6b64c38038cc321
    Former-commit-id: b7d85d425ac55cbc1708fbe7426c0fc1f02e6c22

--- BootStrapScripts/SystemPrep-Bootstrap--Linux.sh
+++ BootStrapScripts/SystemPrep-Bootstrap--Linux.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -ex
+set -e
 
 #User variables
 SOURCEISS3BUCKET="True"
@@ -21,63 +21,63 @@ WORKINGDIR=/usr/tmp
 LOGDIR=/var/log
 LOGTAG=systemprep
 LOGFILE="${LOGDIR}/${LOGTAG}-${TIMESTAMP}.log"
-
-log() {
-    if [[ -n "$1" ]]; then
-        IN="$1"
-    else
-        read IN
-    fi
-    echo ${IN} | tee -a "${LOGFILE}" | "${LOGGER}" -t "${LOGTAG}"
-}
+LOGLINK="${LOGDIR}/${LOGTAG}.log"
 
 # Validate logger exists
 if [[ (! -x ${LOGGER}) || (-z "${LOGGER}") ]]; then
-	echo "Can't find 'logger' in path. Quitting..."
+	echo "Can't find 'logger' in path. Quitting..." > /dev/console 2>&1
 	exit
 fi
 
 # Validate log directory exists
 if [[ ! -d ${LOGDIR} ]]; then
-  echo "Creating ${LOGDIR} directory." 2>&1 | ${LOGGER} -t "${LOGTAG}"
-  mkdir ${LOGDIR} 2>&1 | "${LOGGER}" -t "${LOGTAG}"
+  echo "Creating ${LOGDIR} directory." >(${LOGGER} -t "${LOGTAG}" -s 2> /dev/console) 2>&1
+  mkdir ${LOGDIR} >(${LOGGER} -t "${LOGTAG}" -s 2> /dev/console) 2>&1
 fi
 
 # Validate working directory exists
 if [[ ! -d ${WORKINGDIR} ]]; then
-  echo "Creating ${WORKINGDIR} directory" 2>&1 | "${LOGGER}" -t "${LOGTAG}"
-  mkdir ${WORKINGDIR} 2>&1 | "${LOGGER}" -t "${LOGTAG}"
+  echo "Creating ${WORKINGDIR} directory" >(${LOGGER} -t "${LOGTAG}" -s 2> /dev/console) 2>&1
+  mkdir ${WORKINGDIR} >(${LOGGER} -t "${LOGTAG}" -s 2> /dev/console) 2>&1
 fi
+
+# Establish logging to write to the logfile, syslog, and the console
+exec > >(tee "${LOGFILE}" | "${LOGGER}" -t "${LOGTAG}" -s 2> /dev/console) 2>&1
+
+# Create the link to the logfile
+touch ${LOGFILE}
+ln -s -f ${LOGFILE} ${LOGLINK}
+
 
 # Change to the working directory
 cd ${WORKINGDIR}
 
 # Create the log file and write out the parameters
-echo "Entering SystemPrep script -- ${SCRIPTNAME}" 2>&1 | log
-echo "Writing SystemPrep Parameters to log file..." 2>&1 | log
-for param in "${SYSTEMPREPPARAMS[@]}"; do echo "   ${param}" 2>&1 | log ; done
+echo "Entering SystemPrep script -- ${SCRIPTNAME}"
+echo "Writing SystemPrep Parameters to log file..."
+for param in "${SYSTEMPREPPARAMS[@]}"; do echo "   ${param}" ; done
 
 # Download the master script
 SCRIPTFILENAME=$(echo ${SYSTEMPREPMASTERSCRIPTSOURCE} | awk -F'/' '{ print ( $(NF) ) }')
 SCRIPTFULLPATH=${WORKINGDIR}/${SCRIPTFILENAME}
 if [[ "true" = ${SOURCEISS3BUCKET,,} ]]; then
     if [[ (-x ${AWS}) && (! -z ${AWS}) ]]; then
-        echo "Using AWS Tools to download from S3 Bucket -- ${SYSTEMPREPMASTERSCRIPTSOURCE}" 2>&1 | log
+        echo "Using AWS Tools to download from S3 Bucket -- ${SYSTEMPREPMASTERSCRIPTSOURCE}"
         KEY=$(echo ${SYSTEMPREPMASTERSCRIPTSOURCE} | awk -F'/' '{$1=$2=$3=""; print substr($0,4)}' OFS="/")
-        ${AWS} s3 cp s3://${KEY} ${SCRIPTFULLPATH} --source-region ${AWSREGION} 2>&1 | log
+        ${AWS} s3 cp s3://${KEY} ${SCRIPTFULLPATH} --source-region ${AWSREGION}
     else
-        echo "Missing 'aws' in path. Could not download file. Quitting..." 2>&1 | log
+        echo "Missing 'aws' in path. Could not download file. Quitting..."
         exit
     fi
 else
     if [[ (-x ${CURL}) && (! -z ${CURL}) ]]; then
-        echo "Using 'curl' to download from web host -- ${SYSTEMPREPMASTERSCRIPTSOURCE}" 2>&1 | log
-        ${CURL} -L -O -s -S ${SYSTEMPREPMASTERSCRIPTSOURCE} 2>&1 | log
+        echo "Using 'curl' to download from web host -- ${SYSTEMPREPMASTERSCRIPTSOURCE}"
+        ${CURL} -L -O -s -S ${SYSTEMPREPMASTERSCRIPTSOURCE}
     elif [[ (-x ${WGET}) && (! -z ${WGET}) ]]; then
-        echo "Using 'wget' to download from web host -- ${SYSTEMPREPMASTERSCRIPTSOURCE}" 2>&1 | log
-        ${WGET} --quiet ${SYSTEMPREPMASTERSCRIPTSOURCE} 2>&1 | log
+        echo "Using 'wget' to download from web host -- ${SYSTEMPREPMASTERSCRIPTSOURCE}"
+        ${WGET} --quiet ${SYSTEMPREPMASTERSCRIPTSOURCE}
     else
-        echo "Missing 'curl' or 'wget' in path. Could not download file. Quitting..." 2>&1 | log
+        echo "Missing 'curl' or 'wget' in path. Could not download file. Quitting..."
         exit
     fi
 fi
@@ -86,15 +86,39 @@ fi
 # The string will be converted to a python dictionary by the master script
 PARAMSTRING=$( IFS=$' '; echo "${SYSTEMPREPPARAMS[*]}" )
 
+# Temporarily suppress rsyslog rate limiting
+if [[ -e /etc/rsyslog.conf ]]; then
+    echo "Temporarily disabling rsyslog rate limiting"
+    RSYSLOGFLAG=1
+    # Replace or append the $SystemLogRateLimitInterval parameter
+    grep -q '^$SystemLogRateLimitInterval' /etc/rsyslog.conf && \
+        sed -i.bak -e \
+        "s/^$SystemLogRateLimitInterval.*/$SystemLogRateLimitInterval 0/" \
+        /etc/rsyslog.conf || \
+        sed -i.bak "$ a\$SystemLogRateLimitInterval 0" /etc/rsyslog.conf
+    echo "Restarting rsyslog..."
+    service rsyslog restart
+fi
+
 # Execute the master script
 SCRIPTFILENAME=$(echo ${SYSTEMPREPMASTERSCRIPTSOURCE} | awk -F'/' '{ print ( $(NF) ) }')
 SCRIPTFULLPATH=${WORKINGDIR}/${SCRIPTFILENAME}
-echo "Running the SystemPrep master script -- ${SCRIPTFULLPATH}" 2>&1 | log
-python ${SCRIPTFULLPATH} ${PARAMSTRING} &>> ${LOGFILE}
+echo "Running the SystemPrep master script -- ${SCRIPTFULLPATH}"
+python ${SCRIPTFULLPATH} ${PARAMSTRING}
+
+# Restore prior rsyslog config
+if [[ -n "${RSYSLOGFLAG}" ]]; then
+    # Sleep to let the logger catch up with the output of the python script...
+    sleep 2
+    echo "Re-storing previous rsyslog configuration"
+    mv -f /etc/rsyslog.conf.bak /etc/rsyslog.conf
+    echo "Restarting rsyslog..."
+    service rsyslog restart
+fi
 
 # Cleanup
-echo "Deleting the SystemPrep master script -- ${SCRIPTFULLPATH}" 2>&1 | log
-rm -f ${SCRIPTFULLPATH}  2>&1 | log
+echo "Deleting the SystemPrep master script -- ${SCRIPTFULLPATH}"
+rm -f ${SCRIPTFULLPATH}
 
 # Exit
-echo "Exiting SystemPrep bootstrap script -- ${SCRIPTNAME}" 2>&1 | log
+echo "Exiting SystemPrep bootstrap script -- ${SCRIPTNAME}"
